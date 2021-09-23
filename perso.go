6701d0551bba132816ea23c83dceab76ead19d38
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-09-23 14:37:36 +0200

    Suppression affichage.go

--- perso.go
+++ perso.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Personnage struct {
 	nom        string
 	classe     string
@@ -22,4 +24,14 @@ var P1 Personnage
 
 func PersoInit(p *Personnage) {
 	P1.Init("Byleth", "Roturier", 1, 100, 50, []string{"Epée", "Armure légère", "Potion", "Potion"})
+}
+
+func (p Personnage) DisplayInfo() {
+	fmt.Println("--- Informations du personnage ---")
+	fmt.Println("Nom --> ", p.nom)
+	fmt.Println("Classe --> ", p.classe)
+	fmt.Println("Niveau --> ", p.niveau)
+	fmt.Println("HP maximum --> ", p.hpmax)
+	fmt.Println("HP actuels --> ", p.hp)
+	fmt.Println()
 }
