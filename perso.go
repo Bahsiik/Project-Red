501ad526ef58a22e54b9dc40f659b3bd6c4d9d72
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-10-04 18:31:10 +0200

    Rectification système d'achat + initialisation forgeron

--- perso.go
+++ perso.go
@@ -41,7 +41,7 @@ var Forgeron Personnage
 
 func PersoInit(p *Personnage) { // Fonction pour initialiser les personnages
 <<<<<<< HEAD
-	P1.Init("Byleth", "Roturier", 1, 100, 50, []string{"Epée", "Armure légère", "Potion", "Potion"}, []string{"Coup de poing"}, 100, "", "", "")
+	P1.Init("Byleth", "Roturier", 1, 100, 50, []string{"Epée", "Armure légère", "Potion", "Potion"}, []string{"Coup de poing"}, 100, "chapeau de caca", "torse de caca", "pieds de caca")
 	Marchand.Init("Jeff Besos", "Marchand", 777, 777, 777, []string{"Potion : 3ç", "Potion de poison : 6ç", "Livre de sort: Boule de feu : 25ç", "Fourrure de Loup : 4ç", "Peau de troll : 7ç", "Cuir de Sanglier : 3ç", "Plume de Corbeau : 1ç"}, []string{"Coup de poing"}, 999, "Chapeau Gucci", "Veste Luis Vuitton", "Chaussures Geox")
 	Forgeron.Init("Mickey", "Forgeron", 666, 666, 666, []string{"Chapeau de l'aventurier : 5ç", "Tunique de l'aventurier : 5ç", "Bottes de l'aventurier : 5ç"}, []string{"Coup de poing"}, 666, "", "", "")
 =======
