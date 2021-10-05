3d2ce58d1a5f5415420342b001c2ab4c63d688ad
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-10-05 09:01:39 +0200

    Création préambule fonction craft forgeron

--- forgeron.go
+++ forgeron.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func (p Personnage) DisplayInvForgeron() { // Fonction d'affichage de l'inventaire du forgeron (Articles du magasin)
@@ -14,4 +17,87 @@ func (p Personnage) DisplayInvForgeron() { // Fonction d'affichage de l'inventai
 		}
 	}
 	fmt.Println()
+}
+
+func (p *Personnage) AccessInvForgeron() { //Fonction craft d'objet
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("-> ")
+	textforgeron, _ := reader.ReadString('\n')
+	textforgeron = strings.Replace(textforgeron, "\r\n", "", -1)
+	switch textforgeron {
+	case "Rien": // Retour au menu (aucun objet choisi)
+		fmt.Println("Très bien, passez une bonne journée.")
+		fmt.Println()
+		RetourMenu()
+	case "1": // Craft Chapeau de l'aventurier
+		var test int
+		for i := range p.inventaire { 
+			if i < len(p.inventaire) {
+				if p.inventaire[i] == "Plume de Corbeau : 1ç"  && p.inventaire[i] == "Cuir de Sanglier : 3ç" { // Incrémentattion du compteur de composants par rapport à l'inventaire
+					test++	
+					if (P1.money - 5) >= 0 {
+						fmt.Println("Argent restant : ", P1.money, " Cacas d'or")
+						fmt.Println()
+						ContinueForegronInv("Chapeau de l'aventurier")
+					} else {
+						p.Pauvre()
+					} 
+					}
+				}
+			}
+			if test == 0 { // Dans l'absence des composants dans l'inventaire
+		fmt.Println(p.nom, "n'as malheureusement pas les composants nécessaire...")
+	}
+	case "2" :// Craft Tunique de l'aventurier
+	var test int
+		for i := range p.inventaire { 
+			if i < len(p.inventaire) {
+				if p.inventaire[i] == "Fourrure de Loup : 4ç"  && p.inventaire[i] == "Peau de troll : 7ç" { // Incrémentattion du compteur de composants par rapport à l'inventaire
+					for c := range p.inventaire {
+						if c != i && c < len(p.inventaire[i]) {	
+							if p.inventaire[c] == "Fourrure de Loup : 4ç"{					
+								test++	
+									if (P1.money - 5) >= 0 {
+										fmt.Println("Argent restant : ", P1.money, " Cacas d'or")
+										fmt.Println()
+										ContinueForegronInv("Tunique de l'aventurier")
+									} else {
+										p.Pauvre()
+									}
+								}
+							}
+						} 
+					}
+				}
+			}
+			if test == 0 { // Dans l'absence des composants dans l'inventaire
+		fmt.Println(p.nom, "n'as malheureusement pas les composants nécessaire...")
+	}
+	case "3" : // Craft Bottes de l'aventurier
+		var test int
+		for i := range p.inventaire { 
+			if i < len(p.inventaire) {
+				if p.inventaire[i] == "Cuir de Sanglier : 3ç"  && p.inventaire[i] == "Fourrure de Loup : 4ç" { // Incrémentattion du compteur de composants par rapport à l'inventaire
+					test++	
+					if (P1.money - 5) >= 0 {
+						fmt.Println("Argent restant : ", P1.money, " Cacas d'or")
+						fmt.Println()
+						ContinueForgeronInv("Bottes de l'aventurier")
+					} else {
+						p.Pauvre()
+					} 
+					}
+				}
+			}
+			if test == 0 { // Dans l'absence des composants dans l'inventaire
+		fmt.Println(p.nom, "n'as malheureusement pas les composants nécessaire...")
+	}
+
+}
+
+	case "2" :
+	for i := range p.inventaire {
+		if i < len(p.inventaire) {
+			if p.inventaire[i] == "Potion" { // Incrémentattion du compteur de potions par rapport à l'inventaire
+				test++
 }
--- perso.go
+++ perso.go
@@ -40,10 +40,6 @@ var Marchand Personnage
 var Forgeron Personnage
 
 func PersoInit(p *Personnage) { // Fonction pour initialiser les personnages
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ea2ee74f9420ccfe50c944f246850a1658db7795
 	P1.Init("Byleth", "Roturier", 1, 100, 50, []string{"Epée", "Armure légère", "Potion", "Potion"}, []string{"Coup de poing"}, 100, "chapeau de caca", "torse de caca", "pieds de caca")
 	Marchand.Init("Jeff Besos", "Marchand", 777, 777, 777, []string{"Potion : 3ç", "Potion de poison : 6ç", "Livre de sort: Boule de feu : 25ç", "Fourrure de Loup : 4ç", "Peau de troll : 7ç", "Cuir de Sanglier : 3ç", "Plume de Corbeau : 1ç"}, []string{"Coup de poing"}, 999, "Chapeau Gucci", "Veste Luis Vuitton", "Chaussures Geox")
 	Forgeron.Init("Mickey", "Forgeron", 666, 666, 666, []string{"Chapeau de l'aventurier : 5ç", "Tunique de l'aventurier : 5ç", "Bottes de l'aventurier : 5ç"}, []string{"Coup de poing"}, 666, "", "", "")
