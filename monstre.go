aea5b7186f4680589e2856c664ec22632b32ea43
OMISTRAL <olivier.mistral@ynov.com>
2021-10-05 12:24:23 +0200

    ajout fonction initmonstre

--- monstre.go
+++ monstre.go
@@ -6,3 +6,10 @@ type Monstre struct { // Création structure monstre
 	hp    int
 	atk   int
 }
+
+func (m *Monstre) InitMonstre(nom string, hpmax int, hp int, atk int) {
+	m.nom = nom
+	m.hpmax = hpmax
+	m.hp = hp
+	m.atk = atk
+}
