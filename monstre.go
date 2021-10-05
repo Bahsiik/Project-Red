1eb2aade3a6dde2e1bdec6b8d25dbc80050bf967
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-10-05 16:22:10 +0200

    Initialisation Goblin d'entraînement

--- monstre.go
+++ monstre.go
@@ -13,3 +13,9 @@ func (m *Monstre) InitMonstre(nom string, hpmax int, hp int, atk int) {
 	m.hp = hp
 	m.atk = atk
 }
+
+var Goblin Monstre
+
+func MonstreInit(m *Monstre) {
+	Goblin.InitMonstre("Goblin d'entrainement", 40, 40, 5)
+}
