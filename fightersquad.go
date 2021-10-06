b0b0b6b3b9a4d06f67696cfba5958b3b0aad4b27
OMISTRAL <olivier.mistral@ynov.com>
2021-10-06 09:40:56 +0200

    réponse du gobelin dans le fight

--- fightersquad.go
+++ fightersquad.go
@@ -12,11 +12,14 @@ func TrainingFight(p *Personnage, m *Monstre) { // Initialisation ocmbat d'entra
 	for tour := 1; ; tour++ { // Condition de fin de combat
 		if p.hp > 0 || m.hp > 0 {
 			fmt.Println("======== Tour ", tour, " ========")
+			fmt.Println("C'est à ", p.nom, "d'agir")
+			fmt.Println("Que va faire ", p.nom, " ?")
+			fmt.Println("Rien ou Coup de poing")
 			textfight := Input()
 			switch textfight {
 			case "Rien":
-				fmt.Println("C'est au tour de ", m.nom)
 				fmt.Println()
+				fmt.Println("C'est au ", m.nom, "d'agir")
 				fmt.Println(m.nom, " attaque ", p.nom)
 				p.hp -= 25
 				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp")
@@ -31,8 +34,17 @@ func TrainingFight(p *Personnage, m *Monstre) { // Initialisation ocmbat d'entra
 				m.hp -= 10
 				fmt.Println(m.nom, " a maintenant ", m.hp, " Hp")
 				fmt.Println()
+				fmt.Println("C'est au ", m.nom, "d'agir")
+				fmt.Println(m.nom, " attaque ", p.nom)
+				p.hp -= 25
+				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp")
+				fmt.Println()
 				if m.hp <= 0 {
 					fmt.Println(p.nom, "a gagné le combat :))) uwu")
+					fmt.Println()
+					RetourMenu()
+				} else if p.hp <= 0 {
+					p.Death()
 					fmt.Println()
 					RetourMenu()
 				}
