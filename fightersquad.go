398bd38fd98accc1818df95df01dd6cc755f6b71
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-10-06 10:04:00 +0200

    Commentaire fin de combat d'entrainement

--- fightersquad.go
+++ fightersquad.go
@@ -5,50 +5,52 @@ import (
 	"os"
 )
 
-func TrainingFight(p *Personnage, m *Monstre) { // Initialisation ocmbat d'entrainement
+func TrainingFight(p *Personnage, m *Monstre) { // Initialisation combat d'entrainement
 	fmt.Println(p.nom, " engage le combat d'entrainement")
 	fmt.Println()
 	fmt.Println("FIGHT !!!")
 	for tour := 1; ; tour++ { // Condition de fin de combat
 		if p.hp > 0 || m.hp > 0 {
-			fmt.Println("======== Tour ", tour, " ========")
+			fmt.Println("======== Tour ", tour, " ========") // Initialisation n° de tours
 			fmt.Println("C'est à ", p.nom, "d'agir")
 			fmt.Println("Que va faire ", p.nom, " ?")
-			fmt.Println("Rien ou Coup de poing")
+			fmt.Println("Rien (1) ou Coup de poing (2)") // Choix d'actions du personnage
+			fmt.Println()
 			textfight := Input()
 			switch textfight {
-			case "Rien":
+			case "1":
 				fmt.Println()
-				fmt.Println("C'est au ", m.nom, "d'agir")
+				fmt.Println("C'est au ", m.nom, "d'agir") // Tour du Gobelin
 				fmt.Println(m.nom, " attaque ", p.nom)
-				p.hp -= 25
-				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp")
+				p.hp -= 5
+				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp") // Affichage pv perso fin tour
 				fmt.Println()
-				if p.hp <= 0 {
+				if p.hp <= 0 { // Condition pv à 0 du perso
 					p.Death()
 					fmt.Println()
 					RetourMenu()
 				}
-			case "Coup de poing":
+			case "2":
 				fmt.Println(p.nom, " effectue un Coup de poing")
 				m.hp -= 10
-				fmt.Println(m.nom, " a maintenant ", m.hp, " Hp")
+				fmt.Println(m.nom, " a maintenant ", m.hp, " Hp") // Affichage pv monstre fin tour
 				fmt.Println()
 				fmt.Println("C'est au ", m.nom, "d'agir")
 				fmt.Println(m.nom, " attaque ", p.nom)
-				p.hp -= 25
+				p.hp -= m.atk
 				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp")
 				fmt.Println()
 				if m.hp <= 0 {
-					fmt.Println(p.nom, "a gagné le combat :))) uwu")
+					fmt.Println(p.nom, "a gagné le combat :))) uwu") // Message fin de game
 					fmt.Println()
+					m.hp = m.hpmax // Réinitialisation pv monstre
 					RetourMenu()
 				} else if p.hp <= 0 {
 					p.Death()
 					fmt.Println()
 					RetourMenu()
 				}
-			case "666":
+			case "666": // Fin du monde
 				fmt.Println(" Vous avez détruit le monde Mao-sama !!!!")
 				fmt.Println()
 				os.Exit(0)
