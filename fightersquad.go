07567611fe515534fc9af7b66186d7add833b380
Dxvy <55587711+Dxvy@users.noreply.github.com>
2021-10-06 10:15:58 +0200

    Commentaire fin de combat d'entrainement

--- fightersquad.go
+++ fightersquad.go
@@ -16,17 +16,14 @@ func TrainingFight(p *Personnage, m *Monstre) { // Initialisation combat d'entra
 			fmt.Println("Que va faire ", p.nom, " ?")
 			fmt.Println("Rien (1) ou Coup de poing (2)") // Choix d'actions du personnage
 			fmt.Println()
-			fmt.Println("Rien (1) ou Coup de poing (2)")
 			textfight := Input()
 			switch textfight {
 			case "1":
 				fmt.Println()
 				fmt.Println("C'est au ", m.nom, "d'agir") // Tour du Gobelin
 				fmt.Println(m.nom, " attaque ", p.nom)
-				p.hp -= 5
-				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp") // Affichage pv perso fin tour
 				p.hp -= m.atk
-				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp")
+				fmt.Println(p.nom, " a maintenant ", p.hp, " Hp") // Affichage pv perso fin tour
 				fmt.Println()
 				if p.hp <= 0 { // Condition pv à 0 du perso
 					p.Death()
@@ -37,7 +34,6 @@ func TrainingFight(p *Personnage, m *Monstre) { // Initialisation combat d'entra
 				fmt.Println(p.nom, " effectue un Coup de poing")
 				m.hp -= 10
 				fmt.Println(m.nom, " a maintenant ", m.hp, " Hp") // Affichage pv monstre fin tour
-				fmt.Println(m.nom, " a maintenant ", m.hp, " Hp")
 				if m.hp <= 0 {
 					fmt.Println(p.nom, "a gagné le combat :))) uwu")
 					fmt.Println()
